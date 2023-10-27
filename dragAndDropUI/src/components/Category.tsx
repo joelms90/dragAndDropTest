4c73e55be41884fa62f823b00298566e5747ccc6
joelms90 <joel.morales.unam@gmail.com>
2023-10-27 17:46:23 -0600

    chore: give ability for category to handle drops and render draggable cards

--- dragAndDropUI/src/components/Category.tsx
+++ dragAndDropUI/src/components/Category.tsx
@@ -1,22 +1,50 @@
 import Card from "./Card";
+import { DragAndDropCard } from "./DragAndDropTest";
 
-type CategoryType = {
+export type CategoryComponentType = {
     text: string;
     id: number;
+    cards: DragAndDropCard[];
+    handleOnDrop: any;
+    handleDragOver: any;
+    handleOnDrag: any;
 };
 
-const Category = ({text, id}:CategoryType) => {
-  return (
-  <div className="h-full bg-neutral-900 flex flex-col rounded-lg ">
-    <div key={`${text}-${id}`} className="">
-            <p className="p-4 text-xl font-medium text-slate-100 text-center">{text}</p>
+const Category = ({
+    cards = [],
+    text,
+    id,
+    handleOnDrop,
+    handleDragOver,
+    handleOnDrag,
+}: CategoryComponentType) => {
+    return (
+        <div
+            className="h-full bg-neutral-900 flex flex-col rounded-lg "
+            onDrop={(e) => handleOnDrop(e, id)}
+            onDragOver={(e) => handleDragOver(e, text)}
+            key={`dragZone-${id}`}
+        >
+            <div key={`${text}-${id}`} className="">
+                <p className="p-4 text-xl font-medium text-slate-100 text-center">
+                    {text}
+                </p>
+            </div>
+            <div className="h-full bg-cyan-100 flex flex-col">
+                {cards?.filter((c:any) => c.target === id)?.map(({ id, text, categoryId }: DragAndDropCard) => (
+                    <div className="p-4">
+                        <Card
+                            id={id}
+                            text={text}
+                            categoryId={categoryId}
+                            key={`card-${id}-${text}-category-${categoryId}`}
+                            handleOnDrag={handleOnDrag}
+                        />
+                    </div>
+                ))}
+            </div>
         </div>
-      <div className="h-full bg-cyan-100 flex flex-col">
-        <div className="p-4">
-          <Card text={"test"} categoryId={0}/>
-        </div>
-      </div>
-  </div>)
-}
+    );
+};
 
 export default Category;
