2a9e2de7fcc7ee3ecb24fb66cfec6e9dd409db53
joelms90 <joel.morales.unam@gmail.com>
2023-10-27 17:45:13 -0600

    chore: Make Card able to be draggable

--- dragAndDropUI/src/components/Card.tsx
+++ dragAndDropUI/src/components/Card.tsx
@@ -1,12 +1,20 @@
 type CardType = {
     text: string;
     categoryId: number;
+    id: number;
+    handleOnDrag:any;
 };
 
-const Card = ({text, categoryId}:CardType) => {
-  return <div key={`${text}-${categoryId}`} className="hover:bg-slate-200 hover:cursor-pointer p-6 w-5/6 mx-auto bg-white rounded-xl shadow-lg flex justify-center">
-      <div className="text-xl font-medium text-black">{text}</div>
-  </div>
+const Card = ({text, categoryId, handleOnDrag, id}:CardType) => {
+  return (
+    <div 
+        key={`${text}-${categoryId}`}
+        draggable
+        onDragStart={(e) => handleOnDrag(e, id)}
+        className="hover:bg-slate-200 hover:cursor-grab p-6 w-5/6 mx-auto bg-white rounded-xl shadow-lg flex justify-center">
+        <div className="text-xl font-medium text-black">{text}</div>
+    </div>
+  )
 }
 
 export default Card;
